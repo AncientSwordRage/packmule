ced1cd8cb7d18680e8a99d6b3fc8c0a8c565b85f
AncientSwordRage <pureferret@gmail.com>
2019-06-08 23:40:57 +0100

    Added enzyme/jest tests for AppMain

--- src/AppMain.spec.js
+++ src/AppMain.spec.js
@@ -4,6 +4,7 @@ import AppMain from './AppMain';
 
 import Adapter from 'enzyme-adapter-react-16';
 import { Formik } from 'formik';
+import { Button } from '@material-ui/core';
 Enzyme.configure({ adapter: new Adapter()})
 
 describe('<AppMain />', () => {
@@ -12,7 +13,15 @@ describe('<AppMain />', () => {
         wrapper = shallow(<AppMain />);
     });
     it('Uses formik', () => {
-        console.log(wrapper.debug());
         expect(wrapper.exists(Formik)).toEqual(true)
     });
+    it('Renders a form with a submit button', () => {
+        const renderedForm = wrapper.find(Formik).renderProp('render')({isSubmitting:false});
+        expect(renderedForm.exists('.form-container')).toEqual(true);
+        expect(renderedForm.find(Button).prop('disabled')).toEqual(false);
+    })
+    it('Disables the submit button while submiting', () => {
+        const renderedForm = wrapper.find(Formik).renderProp('render')({isSubmitting:true});
+        expect(renderedForm.find(Button).prop('disabled')).toEqual(true);
+    })
 });
