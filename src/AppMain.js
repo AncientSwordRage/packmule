f90aaad1b349f18c523d4b8d43da7ec5ea3c4471
AncientSwordRage <pureferret@gmail.com>
2019-06-14 00:49:21 +0100

    initial test of jszip

--- src/AppMain.js
+++ src/AppMain.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
+import JSZip from "jszip";
 import { Divider, Button } from '@material-ui/core';
 import { MetaFields } from './FieldComponents/FieldGroups';
 
+//todo replace with webextensions
+function download(filename, base64) {
+  var element = document.createElement('a');
+  element.setAttribute('href', base64);
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+const handleSubmit = (values, { setSubmitting }) => {
+  var zip = new JSZip();
+  const pack = values.pack_mcmeta;
+  setTimeout(() => {
+    zip.file('pack.mcmeta', JSON.stringify({ pack }, null, 2));
+    zip.folder('data');
+    zip.generateAsync({type:"base64"}).then(function (base64) {
+      download(values.pack_name, base64);
+      setSubmitting(false);
+  }, function (err) {
+      alert(err);
+  });
+  }, 400);
+};
 const AppMain = () =>{
     return (
     <main>
@@ -17,17 +46,12 @@ const AppMain = () =>{
             data: {
             }
           }}
-          onSubmit={(values, { setSubmitting }) => {
-           setTimeout(() => {
-             alert(JSON.stringify(values, null, 2));
-             setSubmitting(false);
-           }, 400);
-         }}
+          onSubmit={handleSubmit}
          render={({ isSubmitting }) => (
           <Form className="form-container">
             <MetaFields />
             <Divider variant="middle"></Divider>
-            <Button variant="outlined" type="submit" disabled={isSubmitting}>
+            <Button variant="outlined" download="foo-bar.zip" type="submit" disabled={isSubmitting}>
               Submit
             </Button>
           </Form>
